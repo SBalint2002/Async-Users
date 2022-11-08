4ec56dad86b60849a85156dd325cc4756d9be012
Sárándi <sarandi.balint@gmail.com>
2022-11-08 09:12:48 +0100

    elerhetosegek tablazatban

--- src/index.js
+++ src/index.js
@@ -18,11 +18,54 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
         let adatok = document.getElementById('adatok');
         adatok.textContent = '';
+        let tabla = document.getElementById('table');
+        tabla.innerHTML = "";
 
         for (let p of sorrend){
             let li = document.createElement('li');
-            li.innerHTML = p.lastName.toUpperCase() + " " + p.firstName;
+            li.innerHTML = p.lastName.toUpperCase() + "&emsp;" + p.firstName;
             adatok.appendChild(li);
         }
     });
+
+    document.getElementById('contact').addEventListener('click', async () => {
+        let response = await fetch('./users.json');
+        let eredmeny = await response.json();
+
+        let sorrend = eredmeny.users.sort(function(a, b){
+            let elso = a.username.toUpperCase();
+            let masodik = b.username.toUpperCase();
+
+            if(elso < masodik){
+                return -1;
+            }else if( elso > masodik){
+                return 1;
+            }else{
+                return 0;
+            }
+        });
+
+        let adatok = document.getElementById('adatok');
+        adatok.textContent = '';
+
+        let tabla = document.getElementById('table');
+        tabla.innerHTML = "";
+
+        for (let p of sorrend){
+            let sor = document.createElement('tr');
+
+            let nev = document.createElement('td');
+            let email = document.createElement('td');
+            let phone = document.createElement('td');
+
+            nev.textContent = p.username;
+            email.textContent = p.email;
+            phone.textContent = p.phone;
+
+            sor.appendChild(nev);
+            sor.appendChild(email);
+            sor.appendChild(phone);
+            tabla.appendChild(sor);
+        }
+    })
 })
